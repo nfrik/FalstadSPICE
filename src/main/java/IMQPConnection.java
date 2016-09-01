0f36bf1c0e0752b43aad164d152e89e7f4ea7def
Nikolay Frik <nfrik@iphonedreams.com>
2016-08-31 21:49:36 -0400

    imqp tests updated

--- src/main/java/IMQPConnection.java
+++ src/main/java/IMQPConnection.java
@@ -2,14 +2,14 @@
  * Created by nfrik on 8/31/16.
  */
 
-import com.rabbitmq.client.ConnectionFactory;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.*;
 
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 
 public class IMQPConnection {
+
+    private static final String QUEUE_NAME = "Hyper";
 
     private static IMQPConnection instance = null;
 
@@ -23,21 +23,53 @@ public class IMQPConnection {
         try {
             connection = factory.newConnection();
             channel = connection.createChannel();
-        }catch (TimeoutException e){
+        } catch (TimeoutException e) {
             e.printStackTrace();
         }
     }
 
-    protected IMQPConnection(){
+    protected IMQPConnection() {
 
     }
 
-    public static IMQPConnection getInstance(){
-        if(instance == null){
+    public static void sendToQueue(String data) {
+
+//        String message = "Hello World!";
+        try {
+            channel.basicPublish("", QUEUE_NAME, null, data.getBytes());
+            System.out.println(" [x] Sent '" + data + "'");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void readFromQueue(){
+
+        Consumer consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
+                    throws IOException {
+                String message = new String(body, "UTF-8");
+                System.out.println(" [x] Received '" + message + "'");
+            }
+        };
+        try {
+            channel.basicConsume(QUEUE_NAME, true, consumer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public static IMQPConnection getInstance() {
+        if (instance == null) {
             instance = new IMQPConnection();
+
             try {
                 instance.connectToIMQP();
-
+                channel.queueDeclare(QUEUE_NAME, false, false, false, null);
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -45,4 +77,22 @@ public class IMQPConnection {
         return instance;
     }
 
+    public static void closeConnection() {
+        if (channel != null) {
+            try {
+                channel.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (TimeoutException e) {
+                e.printStackTrace();
+            }
+        }
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
